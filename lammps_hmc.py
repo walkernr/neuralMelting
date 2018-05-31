08f6ef810a6205dc4f32cb2f61b5087ccab5d87e
Nick Walker <walker.nr256@gmail.com>
2018-05-31 08:04:30 -0500

    Add files via upload
    fixed errors with lammps and added adaptive box adjustment for npt-mc

--- lammps_hmc.py
+++ lammps_hmc.py
@@ -29,7 +29,7 @@ n_dat = 64
 name = 'hmc'
 # monte carlo parameters
 mod = 128             # frequency of data storage
-n_swps = 1024*mod      # total hmc sweeps
+n_swps = 1024*mod     # total hmc sweeps
 n_stps = 16           # md steps during hmc
 seed = 256            # random seed
 np.random.seed(seed)  # initialize rng
@@ -63,7 +63,7 @@ lat = {'Ti': ('bcc', 2.951),
        'Al': ('fcc', 4.046),
        'Ni': ('fcc', 3.524),
        'Cu': ('fcc', 3.615),
-       'LJ': ('fcc', 2**(1/6)*lj_param[1])}
+       'LJ': ('fcc', 1.122)}
 # box size
 sz = {'Ti': 5,
       'Al': 4,
@@ -82,12 +82,12 @@ p_hmc = {'Ti': 0.875,
          'Ni': 0.875,
          'Cu': 0.875,
          'LJ': 0.875}
-# max log volume change displacement
-lnvol_max = {'Ti': 0.00048828125,
-             'Al': 0.00048828125,
-             'Ni': 0.00048828125,
-             'Cu': 0.00048828125,
-             'LJ': 0.00024414062}
+# max box adjustment
+dbox = {'Ti': 0.0009765625*lat['Ti'][1],
+        'Al': 0.0009765625*lat['Al'][1],
+        'Ni': 0.0009765625*lat['Ni'][1],
+        'Cu': 0.0009765625*lat['Cu'][1],
+        'LJ': 0.0009765625*lat['LJ'][1]}
 # timestep
 dt = {'real': 4.0,
       'metal': 0.00390625,
@@ -113,6 +113,7 @@ def lammps_input():
     # units and atom style
     lmpsfile.write('units %s\n' % units[el])
     lmpsfile.write('atom_style atomic\n')
+    lmpsfile.write('atom_modify map yes\n\n')
     # construct simulation box
     lmpsfile.write('lattice %s %s\n' % tuple(lat[el]))
     lmpsfile.write('region box block 0 %d 0 %d 0 %d\n' % (3*(sz[el],)))
@@ -138,15 +139,17 @@ def lammps_input():
     if el == 'LJ':
         lmpsfile.write('pair_style lj/cut 2.5\n')
         lmpsfile.write('mass 1 %f\n' % mass[el])
-        lmpsfile.write('pair_coeff * * %f %f\n\n' % lj_param)
+        lmpsfile.write('pair_coeff 1 1 %f %f 2.5\n\n' % lj_param)
     # minimize lattice structure
-    lmpsfile.write('minimize 0.0 1.0e-8 1024 8192\n')
+    lmpsfile.write('fix 1 all box/relax iso %f vmax %f\n' % (P[el], 0.0009765625))
+    lmpsfile.write('minimize 0.0 %f %d %d\n' % (1.49011612e-8, 1024, 8192))
+    lmpsfile.write('unfix 1\n\n')
     # compute kinetic energy
-    lmpsfile.write('compute thermo_ke all ke\n')
+    lmpsfile.write('compute thermo_ke all ke\n\n')
     # dynamics initialization
     lmpsfile.write('timestep %f\n' % dt[units[el]])
     lmpsfile.write('fix 1 all nve\n')
-    lmpsfile.write('run 0\n')
+    lmpsfile.write('run 0')
     # close file and return name
     lmpsfile.close()
     return lmpsfilein
@@ -158,7 +161,7 @@ def lammps_input():
 # generate input file
 lmpsfilein = lammps_input()
 # initialize lammps
-lmps = lammps(name='', cmdargs=['-log', 'none', '-screen', 'none'])
+lmps = lammps(cmdargs=['-log', 'none', '-screen', 'none'])
 lmps.file(lmpsfilein)
 # extract atom information
 natoms = lmps.extract_global('natoms', 0)
@@ -190,7 +193,7 @@ thermo.write('# mintemp:  %f\n' % T[el][0])
 thermo.write('# maxtemp:  %f\n' % T[el][-1])
 thermo.write('# timestep: %f\n' % dt[units[el]])
 thermo.write('# phmc:     %f\n' % p_hmc[el])
-thermo.write('# lnvolmax: %f\n' % lnvol_max[el])
+thermo.write('# dboxmax:  %f\n' % dbox[el])
 thermo.write('# ------------------------------------------------------------------------------------\n')
 thermo.write('# | stp | temp | terr | pe | ke | virial | vol | acchmc | accvol | mdpehmv | mdpevol |\n')
 thermo.write('# ------------------------------------------------------------------------------------\n')
@@ -207,13 +210,12 @@ if units[el] == 'real':
     Pf = 1e-30*(1.01325e5*P[el])/(4.184e3*kB*T[el])  # metropolis prefactor [1/A^3]
 if units[el] == 'metal':
     kB = 8.61733e-5                                  # boltzmann constant [eV/K]
-    Et = natoms*kB*T[el]                             # thermal energy [eV]
+    Et = kB*T[el]                                    # thermal energy [eV]
     Pf = 1e-30*(1e5*P[el])/(1.60218e-19*kB*T[el])    # metropolis prefactor [1/A^3]
 if units[el] == 'lj':
-    # doesn't even stand a chance
-    kB = 1.0
-    Et = natoms*kB*T[el]
-    Pf = P[el]/Et 
+    kB = 1.0                                         # boltzmann constant (normalized and unitless)
+    Et = kB*T[el]                                    # thermal energy [T*]
+    Pf = P[el]/(kB*T[el])                            # metropolis prefactor [1/r*^3]
 
 # -------------------
 # npt-hmc monte carlo
@@ -230,8 +232,8 @@ for i in xrange(len(T[el])):
     tdpehmc = 0
     tdpevol = 0
     # extract initial positions and velocities
-    x = np.ctypeslib.as_array(lmps.gather_atoms('x', 1, 3))
-    v = np.ctypeslib.as_array(lmps.gather_atoms('v', 1, 3))
+    x = np.copy(np.ctypeslib.as_array(lmps.gather_atoms('x', 1, 3)))
+    v = np.copy(np.ctypeslib.as_array(lmps.gather_atoms('v', 1, 3)))
     # extract initial energies
     pe = lmps.extract_compute('thermo_pe', None, 0)/Et[i]
     ke = lmps.extract_compute('thermo_ke', None, 0)/Et[i]
@@ -259,8 +261,8 @@ for i in xrange(len(T[el])):
             # update hmc tries
             ntryhmc += 1
             # save current physical properties
-            x = np.ctypeslib.as_array(lmps.gather_atoms('x', 1, 3))
-            v = np.ctypeslib.as_array(lmps.gather_atoms('v', 1, 3))
+            x = np.copy(np.ctypeslib.as_array(lmps.gather_atoms('x', 1, 3)))
+            v = np.copy(np.ctypeslib.as_array(lmps.gather_atoms('v', 1, 3)))
             pe = lmps.extract_compute('thermo_pe', None, 0)/Et[i]
             ke = lmps.extract_compute('thermo_ke', None, 0)/Et[i]
             etot = pe+ke
@@ -270,8 +272,8 @@ for i in xrange(len(T[el])):
             lmps.command('velocity all zero angular')
             lmps.command('run %d' % n_stps)  # this part should be parallel
             # set new physical properties
-            xnew = np.ctypeslib.as_array(lmps.gather_atoms('x', 1, 3))
-            vnew = np.ctypeslib.as_array(lmps.gather_atoms('v', 1, 3))
+            xnew = np.copy(np.ctypeslib.as_array(lmps.gather_atoms('x', 1, 3)))
+            vnew = np.copy(np.ctypeslib.as_array(lmps.gather_atoms('v', 1, 3)))
             penew = lmps.extract_compute('thermo_pe', None, 0)/Et[i]
             kenew = lmps.extract_compute('thermo_ke', None, 0)/Et[i]
             etotnew = penew+kenew
@@ -290,50 +292,56 @@ for i in xrange(len(T[el])):
                 # revert physical properties
                 lmps.scatter_atoms('x', 1, 3, np.ctypeslib.as_ctypes(x))
                 lmps.scatter_atoms('v', 1, 3, np.ctypeslib.as_ctypes(v))
-                lmps.command('run 0')
         # npt monte carlo
         else:
             # update volume tries
             ntryvol += 1
             # save current physical properties
-            x = np.ctypeslib.as_array(lmps.gather_atoms('x', 1, 3))
+            boxmin = lmps.extract_global('boxlo', 1)
+            boxmax = lmps.extract_global('boxhi', 1)
+            box = boxmax-boxmin
+            vol = np.power(box, 3)
+            x = np.copy(np.ctypeslib.as_array(lmps.gather_atoms('x', 1, 3)))
             pe = lmps.extract_compute('thermo_pe', None, 0)/Et[i]
-            box = np.power(vol, 1.0/3.0)
             # save new physical properties
-            lnvol = np.log(vol)+(np.random.rand()-0.5)*lnvol_max[el]
-            volnew = np.exp(lnvol)
-            boxnew = np.power(volnew, 1.0/3.0)
+            boxnew = box+(np.random.rand()-0.5)*dbox[el]
+            volnew = np.power(boxnew, 3)
             scalef = boxnew/box
-            xnew = boxnew/box*x
+            xnew = scalef*x
             # apply new physical properties
-            lmps.command('change_box all x final 0.0 %.10f y final 0.0 %.10f z final 0.0 %.10f units box' % (3*(boxnew,)))
+            lmps.command('change_box all x final 0.0 %f y final 0.0 %f z final 0.0 %f units box' % (3*(boxnew,)))
             lmps.scatter_atoms('x', 1, 3, np.ctypeslib.as_ctypes(xnew))
             lmps.command('run 0')
             penew = lmps.extract_compute('thermo_pe', None, 0)/Et[i]
             # calculate enthalpy criterion
-            dH = (penew-pe)+Pf[i]*(volnew-vol)-(natoms+1.)*np.log(volnew/vol)
+            dH = (penew-pe)+Pf[i]*(volnew-vol)-natoms*np.log(volnew/vol)
             if np.random.rand() <= np.min([1, np.exp(-dH)]):
                 # update volume acceptations
                 naccvol += 1
                 tdpevol += penew-pe
                 # save new physical properties
-                pe = penew
-                vol = volnew
                 box = boxnew
+                vol = volnew
                 x = xnew
+                pe = penew
             else:
                 # revert physical properties
+                lmps.command('change_box all x final 0.0 %f y final 0.0 %f z final 0.0 %f units box' % (3*(box,)))
                 lmps.scatter_atoms('x', 1, 3, np.ctypeslib.as_ctypes(x))
-                lmps.command('change_box all x final 0.0 %.10f y final 0.0 %.10f z final 0.0 %.10f units box' % (3*(box,)))
                 lmps.command('run 0')
-        # data storage
+        # max box adjustment update and data storage
         if (j+1) % mod == 0:
             # acceptance ratios
-            acchmc = np.nan_to_num(np.float64(nacchmc)/ntryhmc)
-            accvol = np.nan_to_num(np.float64(naccvol)/ntryvol)
+            acchmc = np.nan_to_num(np.float64(nacchmc)/np.float64(ntryhmc))
+            accvol = np.nan_to_num(np.float64(naccvol)/np.float64(ntryvol))
+            # update max box adjustment
+            if accvol < 0.5:
+                dbox[el] *= 0.9375
+            else:
+                dbox[el] *= 1.0625
             # mean mc arguments
-            mdpehmc = np.nan_to_num(np.float64(tdpehmc)/nacchmc)
-            mdpevol = np.nan_to_num(np.float64(tdpevol)/naccvol)
+            mdpehmc = np.nan_to_num(np.float64(tdpehmc)/np.float64(nacchmc))
+            mdpevol = np.nan_to_num(np.float64(tdpevol)/np.float64(naccvol))
             # calculate physical properties
             virial = lmps.extract_compute('thermo_press', None, 0)
             temp = lmps.extract_compute('thermo_temp', None, 0)
@@ -353,3 +361,5 @@ for i in xrange(len(T[el])):
 # close data storage files
 thermo.close()
 traj.close()
+#close lammps
+lmps.close()
